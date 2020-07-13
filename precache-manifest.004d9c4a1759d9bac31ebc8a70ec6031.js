321827330796e2e09f9d88b586462af8a798e87c
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-13 14:56:36 +0530

    Updates

---
+++ precache-manifest.004d9c4a1759d9bac31ebc8a70ec6031.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03605e61b4fa4f2cf7cf13c24f30899",
+    "url": "/updatenew/index.html"
+  },
+  {
+    "revision": "cfdb89b2ce7a16fb6c81",
+    "url": "/updatenew/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a1d6262876cfce5301fe",
+    "url": "/updatenew/static/css/main.6774804c.chunk.css"
+  },
+  {
+    "revision": "cfdb89b2ce7a16fb6c81",
+    "url": "/updatenew/static/js/2.b94b8ee8.chunk.js"
+  },
+  {
+    "revision": "1c9c054c6a8dacee8dc69ecc1cf8f242",
+    "url": "/updatenew/static/js/2.b94b8ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d6262876cfce5301fe",
+    "url": "/updatenew/static/js/main.3c69f7af.chunk.js"
+  },
+  {
+    "revision": "ba67ff13bf22af43bf08",
+    "url": "/updatenew/static/js/runtime-main.84f7e61d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/updatenew/static/media/logo.5d5d9eef.svg"
+  }
+]);
